1ad0eefb94dd7df8076cc0897071baaedf2cfb43
ahsanMah <mahmood.ahsan@outlook.com>
2025-02-21 15:25:15 -0500

    Add Embedder class for text embedding and k-nearest neighbors functionality

---
+++ ragged/embedding.py
@@ -0,0 +1,46 @@
+from typing import List, Literal, Union
+
+import torch
+from sentence_transformers import SentenceTransformer
+from sklearn.neighbors import NearestNeighbors
+
+from config import Config
+
+
+class Embedder:
+
+    def __init__(
+        self,
+        device: Union[str, torch.device],
+        model_dir: str = Config.EMBEDDING_MODEL_DIR,
+        prompt_name: Literal["s2p_query", "s2s_query"] = "s2s_query",
+    ):
+        device = torch.device(device)
+        self.model = SentenceTransformer(
+            "NovaSearch/stella_en_400M_v5",
+            trust_remote_code=True,
+            device=device,
+            config_kwargs={
+                "use_memory_efficient_attention": device.type == "cuda",
+                "unpad_inputs": device.type == "cuda",
+            },
+        )
+
+    def embed(self, data: List[str]):
+        """
+        Embeds the data into a vector space using a predefined model.
+        """
+        embeddings = self.model.encode(data, show_progress_bar=True)
+        return embeddings
+
+    def embed_and_persist(self, data: List[str], path: str):
+        """"""
+        pass
+
+    def k_nearest_neighbors(self, embeddings, query: str, k: int = 5):
+        """"""
+        knn = NearestNeighbors(n_neighbors=5, metric="cosine")
+        knn.fit(embeddings)
+        query_embedding = self.model.encode([query])
+        indices = knn.kneighbors(query_embedding, return_distance=False)
+        return indices.flatten()
